8ba311ac490360eafafe9e300bd6ef0d5264f3b6
leeyourimm <147682810+leeyourimm@users.noreply.github.com>
2025-08-21 17:56:03 +0900

    Add files via upload

---
+++ LookLook-Spring/demo/LookLook-WebApplication/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyles from "./styles/GlobalStyles";
+
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <>
+    <GlobalStyles />
+      <Provider store={store}>
+          <App />
+      </Provider>
+  </>
+  //</React.StrictMode>
+);
+
+
+reportWebVitals();
